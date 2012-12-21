44aeb6f9887eb1060554bccbc0ea2c6e1d5cad35
apalagummi <aditya2510@gmail.com>
2012-12-21 15:43:46 +0530

    added the example of routes file contaning 61k lines of data with a few hunderd airport suffix codes.

---
+++ noisefilters/src/main/java/com/noisefilter/stopfilter/CommonParameters.java
@@ -0,0 +1,5 @@
+package com.noisefilter.stopfilter;
+
+public final class CommonParameters {
+ public static String dataPath= "/home/adityap/tmp/";
+}
--- noisefilters/src/main/java/com/noisefilter/stopfilter/Driver.java
+++ noisefilters/src/main/java/com/noisefilter/stopfilter/Driver.java
@@ -1,20 +1,47 @@
 package com.noisefilter.stopfilter;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Driver {
 	private static StopFilterFarm filterfarm = new StopFilterFarm();
 
-	public static void main(String args[]) {
-		Scanner sc = new Scanner( Driver.class.getResourceAsStream("stopwords.txt"));
-		while (sc.hasNext()) {
-			String s = sc.next();
-			//System.out.println(s);
-			filterfarm.addTree(s);
+	public static void main(String args[]) throws Exception {
+		File file = new File(CommonParameters.dataPath+"stopwords.txt");
+//		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+//		BufferedWriter bw = new BufferedWriter(fw);
+//		Scanner sc = new Scanner( Driver.class.getResourceAsStream("routes.txt"));
+//		int count = 0;
+//		while (sc.hasNext()) {
+//			String s = sc.next();
+//			count++;
+//			bw.write(s.substring(0,8)+"\n") ;
+//			//System.out.println(s.substring(0,8));
+//			//filterfarm.addTree(s);
+//		}
+//		//System.out.println(count);
+//		bw.close();
+//		sc.close();
+		Scanner sc_stop = new Scanner(file);
+		while (sc_stop.hasNext()) {
+			String s = sc_stop.next();
+			filterfarm.addTree(s);		
 		}
-		sc.close();
-		filterfarm.dump();
-		System.out.println(filterfarm.searchTree("the"));
+		sc_stop.close();
+		Scanner sc_data = new Scanner(Driver.class.getResourceAsStream("routes.txt"));
+		while (sc_data.hasNext()) {
+			String s = sc_data.next();
+			if(filterfarm.searchTree(s.substring(0,8))){
+				System.out.println(s.substring(8));
+			}	
+			else
+				System.out.println(s);
+		}
+		sc_data.close();
+		//filterfarm.dump();
+		//System.out.println(filterfarm.searchTree("ZL4178SY"));
 		//System.out.println(filterfarm.searchTree("alwaresderewdfa"));
 		
 	}
